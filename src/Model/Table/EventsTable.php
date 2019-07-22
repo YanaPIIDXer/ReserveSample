7ac607c2ddb05e83c5aacdbc95ea88152d8d9f36
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-22 19:59:47 +0900

    イベントテーブル、Model定義。

---
+++ src/Model/Table/EventsTable.php
@@ -0,0 +1,73 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Events Model
+ *
+ * @method \App\Model\Entity\Event get($primaryKey, $options = [])
+ * @method \App\Model\Entity\Event newEntity($data = null, array $options = [])
+ * @method \App\Model\Entity\Event[] newEntities(array $data, array $options = [])
+ * @method \App\Model\Entity\Event|bool save(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Event saveOrFail(\Cake\Datasource\EntityInterface $entity, $options = [])
+ * @method \App\Model\Entity\Event patchEntity(\Cake\Datasource\EntityInterface $entity, array $data, array $options = [])
+ * @method \App\Model\Entity\Event[] patchEntities($entities, array $data, array $options = [])
+ * @method \App\Model\Entity\Event findOrCreate($search, callable $callback = null, $options = [])
+ */
+class EventsTable extends Table
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->setTable('events');
+        $this->setDisplayField('name');
+        $this->setPrimaryKey('id');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->integer('id')
+            ->allowEmptyString('id', null, 'create');
+
+        $validator
+            ->scalar('name')
+            ->maxLength('name', 255)
+            ->requirePresence('name', 'create')
+            ->notEmptyString('name');
+
+        $validator
+            ->scalar('description')
+            ->requirePresence('description', 'create')
+            ->notEmptyString('description');
+
+        $validator
+            ->integer('capacity')
+            ->requirePresence('capacity', 'create')
+            ->notEmptyString('capacity');
+
+        $validator
+            ->dateTime('date')
+            ->requirePresence('date', 'create')
+            ->notEmptyDateTime('date');
+
+        return $validator;
+    }
+}
