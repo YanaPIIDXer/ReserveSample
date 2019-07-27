3a3a8abb5bf2319b4e9a818b0bd08fc2b9061aa6
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2019-07-27 09:34:53 +0900

    タイムゾーンの変更。

--- config/app.php
+++ config/app.php
@@ -270,7 +270,7 @@ return [
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
             //'encoding' => 'utf8mb4',
-            'timezone' => 'UTC',
+            'timezone' => 'Asia/Tokyo',
             'flags' => [],
             'cacheMetadata' => true,
             'log' => false,
@@ -310,7 +310,7 @@ return [
             'password' => 'secret',
             'database' => 'test_myapp',
             //'encoding' => 'utf8mb4',
-            'timezone' => 'UTC',
+            'timezone' => 'Asia/Tokyo',
             'cacheMetadata' => true,
             'quoteIdentifiers' => false,
             'log' => false,
